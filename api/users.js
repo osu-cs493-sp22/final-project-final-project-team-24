01c12a29793eb85ef458b3cee3e3ee192b96d92a
Ziwen Tong <tongzi@oregonstate.edu>
2022-06-08 23:27:36 -0700

    POST/users, POST/users/login, GET/users/{id} done

--- api/users.js
+++ api/users.js
@@ -1,2 +1,79 @@
 const { Router } = require('express')
-module.exports = Router
+const { validateAgainstSchema } = require('../lib/validation')
+const bcrypt = require('bcryptjs')
+const { generateAuthToken, requireAuthentication } = require('../lib/auth')
+const {
+    UserSchema,
+    insertNewUser,
+    getUserById
+} = require('../models/user')
+
+const router = Router()
+
+
+/*
+ * POST /users - Create a new user.
+ */
+router.post('/', async (req, res) => {
+    if (validateAgainstSchema(req.body, UserSchema)) {
+        const id = await insertNewUser(req.body)
+        res.status(201).send({
+            _id: id
+        })
+    } else {
+        res.status(400).send({
+            error: "Request body does not contain a valid User."
+        })
+    }
+})
+
+/*
+ * POST /users/login - Log in a User.
+ */
+router.post('/login', async  (req, res) =>  {
+    if (req.body && req.body.id && req.body.email &&req.body.password) {
+        const user = await getUserById(req.body.id, true)
+        const authenticated = user && await bcrypt.compare(
+            req.body.password,
+            user.password
+        )
+        if (authenticated) {
+            const token = generateAuthToken(req.body.id)
+            res.status(200).send({ token: token })
+        } else {
+            res.status(401).send({
+                error: "Invalid credentials"
+            })
+        }
+    } else {
+        res.status(400).send({
+            error: "Request needs email and password."
+        })
+    }
+})
+
+/*
+ * Get /users/{id} - Fetch data about a specific user.
+ */
+router.get('/:id', requireAuthentication, async (req, res, next) => {
+    console.log("== req.user:", req.user)
+    if (req.user !== req.params.id) {
+        res.status(403).send({
+          err: "Unauthorized to access the specified resource"
+        })
+        next()
+    } else {
+        const user = await getUserById(req.params.id)
+        console.log("== req.headers:", req.headers)
+        if (user) {
+            res.status(200).send(user)
+        } else {
+            next()
+        }
+    }
+})
+
+
+
+
+module.exports = router
--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ const { ObjectId, GridFSBucket } = require('mongodb');
 
 const { getDbReference } = require('../lib/mongo')
 const { extractValidFields } = require('../lib/validation')
+const bcrypt = require('bcryptjs')
+const { generateAuthToken, requireAuthentication } = require('../lib/auth')
 
 const UserSchema = {
     name: { required: true },
@@ -10,6 +12,7 @@ const UserSchema = {
     role: { required: true }
 }
 
+exports.UserSchema = UserSchema
 
 
 async function bulkInsertNewUsers(users) {
@@ -22,3 +25,38 @@ async function bulkInsertNewUsers(users) {
     return result.insertedIds
 }
 exports.bulkInsertNewUsers = bulkInsertNewUsers
+
+/*
+ * Create and store a new application User with specified data and 
+ * adds it to the application's database. Only an authenticated User 
+ * with 'admin' role can create users with the 'admin' or 'instructor' roles.
+ */
+async function insertNewUser(user){
+    const userToInsert = extractValidFields(user, UserSchema)
+    userToInsert.password = await bcrypt.hash(userToInsert.password, 8)
+    console.log("== Hashed, salted password:", userToInsert.password)
+    const db = getDbReference()
+    const collection = db.collection('users')
+    const result = await collection.insertOne(userToInsert)
+    return result.insertedId
+}
+exports.insertNewUser = insertNewUser
+
+/*
+ * fetch the user entry (with password) corresponding to the user ID 
+ * specified in the request body.
+ */
+async function getUserById(id, includePassword){
+    const db = getDbReference()
+    const collection = db.collection('users')
+    if (!ObjectId.isValid(id)) {
+        return null
+    } else {
+        const results = await collection
+            .find({ _id: new ObjectId(id) })
+            .project(includePassword ? {} : { password: 0 })
+            .toArray()
+        return results[0]
+    }
+}
+exports.getUserById = getUserById
