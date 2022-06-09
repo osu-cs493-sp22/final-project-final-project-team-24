c4c54a5b422a0dcaf4a2f6a9d90485677b7fea08
Ziwen Tong <tongzi@oregonstate.edu>
2022-06-08 22:34:44 -0700

    POST, GET, PATCH, DELETE / assignments/{id} done

--- api/assignments.js
+++ api/assignments.js
@@ -1,2 +1,94 @@
 const { Router } = require('express')
-module.exports = Router
+const { validateAgainstSchema } = require('../lib/validation')
+const {
+    AssignmentSchema,
+    insertNewAssignment,
+    getAssignmentById,
+    UpdateOneAssignment,
+    DeleteOneAssignment
+} = require('../models/assignment')
+
+const router = Router()
+
+
+/*
+ * POST /assignments - Route to create a new assignment.
+ */
+router.post('/', async (req, res) => {
+    if (validateAgainstSchema(req.body, AssignmentSchema)) {
+        try {
+            const id = await insertNewAssignment(req.body)
+            res.status(201).send({
+                id: id
+            })
+        } catch (err) {
+            console.error(err)
+            res.status(500).send({
+                error: "Error inserting assignment into DB.  Please try again later."
+            })
+        }
+    } else {
+        res.status(400).send({
+            error: "Request body is not a valid assignment object."
+        })
+    }
+})
+
+/*
+ * GET /assignments/{id} - Route to fetch data about a specific assignment.
+ */
+router.get('/:id', async (req, res, next) => {
+    try {
+        const assignment = await getAssignmentById(req.params.id)
+        if (assignment) {
+            res.status(200).send(assignment)
+        } else {
+            next()
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(500).send({
+            error: "Unable to fetch assignment.  Please try again later."
+        })
+    }
+})
+
+/*
+ * PATCH /assignments/{id} - Update data for a specific Assignment.
+ */
+router.patch('/:id', async (req, res, next) =>{
+    try {
+        const assignment = await UpdateOneAssignment(req.params.id, req.body)
+        if (assignment) {
+            res.status(200).send(assignment)
+        } else {
+            next()
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(500).send({
+            error: "Unable to update assignment.  Please try again later."
+        })
+    }
+})
+
+/*
+ * DELETE /assignments/{id} - Remove a specific Assignment from the database.
+ */
+router.delete('/:id', async (req, res, next) =>{
+    try {
+        const assignment = await DeleteOneAssignment(req.params.id)
+        if (assignment) {
+            res.status(200).send(assignment)
+        } else {
+            next()
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(500).send({
+            error: "Unable to delete assignment.  Please try again later."
+        })
+    }
+})
+
+module.exports = router
---
+++ models/assignment.js
@@ -0,0 +1,79 @@
+const { ObjectId } = require('mongodb')
+
+const { getDbReference } = require('../lib/mongo')
+const { extractValidFields } = require('../lib/validation')
+
+const AssignmentSchema = {
+    courseId: { required: true },
+    title: { required: true },
+    points: { required: true },
+    due: { required: true}
+}
+
+exports.AssignmentSchema = AssignmentSchema
+
+/*
+ * Create and store a new Assignment with specified data and adds it to the application's database. 
+ * Only an authenticated User with 'admin' role or an authenticated 'instructor' User whose ID matches 
+ * the instructorId of the Course corresponding to the Assignment's courseId can create an Assignment.
+ */
+async function insertNewAssignment(assignment) {
+    const db = getDbReference()
+    const collection = db.collection('assignments')
+    
+    assignment = extractValidFields(assignment, AssignmentSchema)
+    const result = await collection.insertOne(assignment)
+    return result.insertedId
+}
+exports.insertNewAssignment = insertNewAssignment
+
+/*
+ * Returns summary data about the Assignment, excluding the list of Submissions
+ */
+async function getAssignmentById(id){
+    const db = getDbReference()
+    const collection = db.collection('assignments')
+    const assignments = await collection.find({
+        _id: new ObjectId(id)
+    }).toArray()  
+    return assignments[0];
+}
+exports.getAssignmentById = getAssignmentById
+
+/*
+ * Performs a partial update on the data for the Assignment. Note that submissions 
+ * cannot be modified via this endpoint. Only an authenticated User with 'admin' role or 
+ * an authenticated 'instructor' User whose ID matches the instructorId of the Course 
+ * corresponding to the Assignment's courseId can update an Assignment.
+ */
+async function UpdateOneAssignment(id, assignment){
+    const db = getDbReference()
+    const collection = db.collection('assignments')
+    const assignments = await collection.replaceOne(
+        { _id: new ObjectId(id)},
+        {
+            courseId: assignment.courseId,
+            title: assignment.title,
+            points: assignment.points,
+            due: assignment.due
+        }
+    )
+    return assignments
+}
+exports.UpdateOneAssignment = UpdateOneAssignment
+
+/*
+ * Completely removes the data for the specified Assignment, including all 
+ * submissions. Only an authenticated User with 'admin' role or an authenticated 
+ * 'instructor' User whose ID matches the instructorId of the Course corresponding to 
+ * the Assignment's courseId can delete an Assignment.
+ */
+async function DeleteOneAssignment(id){
+    const db = getDbReference()
+    const collection = db.collection('assignments')
+    const assignments = await collection.deleteOne({
+        _id: new ObjectId(id)
+    })
+    return assignments
+}
+exports.DeleteOneAssignment = DeleteOneAssignment
